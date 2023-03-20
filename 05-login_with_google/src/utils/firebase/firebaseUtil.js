8d1d1e24bb668fe207dbf024220ce8d468eb9a0f
Ying Chiu Goh <YingG@whiteclife.ac.nz>
2023-03-21 08:39:34 +1300

    log in with google react front end

---
+++ 05-login_with_google/src/utils/firebase/firebaseUtil.js
@@ -0,0 +1,61 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJJNuU3QAOdzeGNgstUp9LtGxtg3WRwt0",
+  authDomain: "sandwich-app-808be.firebaseapp.com",
+  projectId: "sandwich-app-808be",
+  storageBucket: "sandwich-app-808be.appspot.com",
+  messagingSenderId: "986105410238",
+  appId: "1:986105410238:web:1bbde6781349d4fb4364ef",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  // if user data is not exits
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  // if user data exits
+  // return userDoc Ref
+  return userDocRef;
+};
